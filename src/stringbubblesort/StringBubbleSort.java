50baca863c558b1f29de39293d0dd426de8c16f8
aksdev07 <beta4bug@gmail.com>
2019-10-01 16:37:23 +0530

    Commiting the bubblesort for String array : minor enancements used comtemporary for each for displaying string array

--- src/stringbubblesort/StringBubbleSort.java
+++ src/stringbubblesort/StringBubbleSort.java
@@ -12,10 +12,6 @@ public class StringBubbleSort {
                         x[j] = x[i];
                         x[i] = temp;
                     }
-
-
-
-
                 }
             }
             return x;
@@ -24,14 +20,17 @@ public class StringBubbleSort {
         public static void main(String[] args) {
             String[] a={"Now", "is", "time","for","all","good","men","to","come","to", "the"  ,"aid","of","their","country"};
             StringBubbleSort ref=new StringBubbleSort();
-            for(int i=0;i<a.length;i++){
-                //System.out.println(a[i]);
-            }
+
+            for(String x:a)
+                System.out.print(x+" ");
             System.out.println(" ");
+
             a=ref.bSortMethod(a);
-            for(int i=0;i<a.length;i++){
-                System.out.println(a[i]);
+
+            for (String x:a){
+                System.out.print(x+" ");
             }
+
 
         }
     }
