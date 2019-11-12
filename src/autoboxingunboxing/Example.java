f44f888f09286035b8c332dd8239dd2de1b0b1b7
aksdev07 <beta4bug@gmail.com>
2019-11-12 17:34:25 +0530

    autoboxing and unboxing example
    Signed-off-by: aksdev07 <beta4bug@gmail.com>

---
+++ src/autoboxingunboxing/Example.java
@@ -0,0 +1,10 @@
+package autoboxingunboxing;
+
+public class Example {
+    public static void main(String[] args) {
+        Integer a=100;
+        int b=a;
+        System.out.println(a+" "+b);
+
+    }
+}
