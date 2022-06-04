bda2e1fef62083fb9121e5d1505edf7d852f21a5
anujk <anujkaushik15@gmail.com>
2022-06-04 20:48:52 +0530

    WAP: Find if two strings are anagram of each other.

---
+++ src/WAP/Anagram.java
@@ -0,0 +1,30 @@
+package WAP;
+
+import java.util.HashMap;
+
+/*
+*WAP to find if two string are anagram of each other
+* */
+public class Anagram {
+    HashMap<Character,Integer> h1=new HashMap<>();
+    HashMap<Character,Integer> h2=new HashMap<>();
+    String s1,s2;
+    Anagram(String S1, String S2){
+        this.s1=S1;
+        this.s2=S2;
+    }
+
+    public static void main(String[] args) {
+        Anagram Obj = new Anagram("aabb","bbaa");
+        for (Character c:Obj.s1.toCharArray()){
+            Obj.h1.put(c,Obj.h1.getOrDefault(c,0));
+        }
+        for (Character c:Obj.s2.toCharArray()){
+            Obj.h2.put(c,Obj.h2.getOrDefault(c,0));
+        }
+        if (Obj.h1.equals(Obj.h2)){
+            System.out.println("YES");
+        }else
+            System.out.println("NO");
+    }
+}
