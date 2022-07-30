5a9be45e108892fd9280c4810809bb39293a1ed2
anujk <anujkaushik15@gmail.com>
2022-07-30 18:18:56 +0530

    WAP: fix related to hashmap put

---
+++ src/filesandfolders/FolderDriver.java
@@ -0,0 +1,10 @@
+package filesandfolders;
+
+import java.io.IOException;
+
+public class FolderDriver extends FolderManager {
+    public static void main(String[] args) throws IOException {
+        FolderManager obj = new FolderManager();
+        obj.inputFolderName();
+    }
+}
---
+++ src/filesandfolders/FolderManager.java
@@ -0,0 +1,65 @@
+package filesandfolders;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class FolderManager {
+    /**
+     * user.home, user.os, user.dir
+     */
+    private String targetPath = System.getProperty("user.home") + "\\Desktop\\Test\\";
+    private String currentPath = System.getProperty("user.dir") + "\\src\\filesandfolders\\Test\\";
+    private Scanner sc = new Scanner(System.in);
+    private String folderName;
+    private String subFolder;
+    private String currentDateFolder;
+    FolderManager(){
+        System.out.println("Enter the name of the desired a directory: ");
+        currentDateFolder = sc.next();
+    }
+
+    public void createFolder(String fName) {
+        System.out.println(targetPath);
+//        System.out.println("Enter the name of the desired a directory: ");
+//        folderName = targetPath + sc.next();
+        folderName = targetPath + fName;
+        File file = new File(folderName);
+        boolean bool = file.mkdir();
+        if (bool) {
+            System.out.println("Folder is created successfully");
+        } else {
+            System.out.println("Error Found!");
+        }
+        createSubFolder(fName);
+    }
+
+    public void inputFolderName() throws IOException {
+        //File file = new File(currentPath + "foldernames.txt");
+        BufferedReader br = new BufferedReader(new FileReader(currentPath + "foldernames"));
+        String st;
+        while ((st = br.readLine()) != null)
+            createFolder(st);
+        System.out.println(st);
+
+    }
+
+    private void createSubFolder(String fName) {
+
+        folderName = targetPath + fName+"\\"+currentDateFolder;
+        //subFolder = folderName;
+        File file = new File(folderName);
+        boolean bool = file.mkdir();
+        if (bool) {
+            System.out.println("Folder is created successfully");
+        } else {
+            System.out.println("Error Found!");
+        }
+    }
+}
+
+
+
+
