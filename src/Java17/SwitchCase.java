45cee6f402c88a864d19dce8f55bcb65b74cc947
anujkaushik <anujkaushik15@gmail.com>
2024-06-02 01:15:36 +0530

    [Added] : Java 17 Switch cases comments

--- src/Java17/SwitchCase.java
+++ src/Java17/SwitchCase.java
@@ -2,6 +2,10 @@ package Java17;
 
 public class SwitchCase {
     public static void main(String[] args) {
+        /*Java 17 has this switch case syntax change which looks cool in this switch case can be used
+        * as an expression and switch case can return a value to the variable notice the semicolon at
+        * the end of the switch case, also instead of break one can use yeild keyword in java 17
+        * for switch cases */
         String day = "Monday";
         String result = switch (day){
             case "Monday" ->  "6am";
